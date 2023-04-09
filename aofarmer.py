6916b2d611306d1883b8d4094700ae48a0ce2b2b
dearfad <dearfad@sina.com>
2023-04-09 22:06:55 +0800

    Create aofarmer.py

---
+++ aofarmer.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title='Albion Online Farmer', page_icon='👨‍🌾', layout="wide")
+
+api_url = "https://east.albion-online-data.com/api/v2/stats/"
+
+st.write("# Albion Online Farmer! 👨‍🌾")
