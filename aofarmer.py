0e7c83f00dd50e1b456083a28a18eb9fd4dca36c
dearfad <dearfad@sina.com>
2023-04-10 08:22:16 +0800

    Update aofarmer.py

--- aofarmer.py
+++ aofarmer.py
@@ -8,7 +8,7 @@ api_url = "https://east.albion-online-data.com/api/v2/stats/"
 
 st.write("# Albion Online Farmer! 👨‍🌾")
 
-item = api_url + 'history/T4_BAG?time-scale=1'
+item = api_url + 'history/T4_BAG.json?locations=Caerleon&qualities=1&time-scale=1'
 r = requests.get(item)
 price = pd.DataFrame(r.json())
 st.write(price)
