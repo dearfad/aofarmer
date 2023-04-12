64c7705e827ed84621341a43e659fbf5b890a0e8
dearfad <dearfad@sina.com>
2023-04-12 09:35:28 +0800

    update

--- aofarmer.py
+++ aofarmer.py
@@ -4,11 +4,14 @@ import requests
 
 st.set_page_config(page_title='Albion Online Farmer', page_icon='👨‍🌾', layout="wide")
 
-api_url = "https://east.albion-online-data.com/api/v2/stats/"
+api_url = "https://east.albion-online-data.com/api/v2/stats"
 
 st.write("# Albion Online Farmer! 👨‍🌾")
 
-item = api_url + 'history/T4_BAG.json?locations=Caerleon&qualities=1&time-scale=1'
+item = api_url + '/api/v2/stats/prices/T4_BAG,T5_BAG.json?locations=Caerleon,Bridgewatch&qualities=2'
+
 r = requests.get(item)
+
 price = r.json()
+
 st.write(price)
