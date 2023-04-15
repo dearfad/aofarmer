f3484bc3a53ff627802ae8989e8b945d6746ae8e
dearfad <dearfad@sina.com>
2023-04-15 18:02:58 +0800

    update

--- aofarmer.py
+++ aofarmer.py
@@ -33,7 +33,7 @@ with col_search:
 
 with col_result:
     search_result = item_ids[item_ids['UniqueName'].str.contains(input_name.strip(), case=False) | item_ids['Name'].str.contains(input_name.strip(), case=False)]
-    selected_item = st.selectbox('搜索结果：', search_result['Name'] + ' ' + search_result['UniqueName'] + search_result['Description'])
+    selected_item = st.selectbox('搜索结果：', search_result['Name'] + ' ' + search_result['UniqueName'] + ' ' + search_result['Description'])
 
 col_item_info, col_item_price = st.columns([1,3])
 
