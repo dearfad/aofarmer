deedba2c61c0bc827f768de8a1426b294581cfdd
dearfad <dearfad@sina.com>
2023-04-15 16:58:14 +0800

    UPDATE

--- aofarmer.py
+++ aofarmer.py
@@ -19,8 +19,8 @@ def read_items_info():
     st.write(ao_bin_dumps_df)
     item_ids_df = pd.DataFrame()
     item_ids_df['UniqueName'] = ao_bin_dumps_df['UniqueName']
-    item_ids_df['Name'] = ao_bin_dumps_df['LocalizedNames'].values
-    item_ids_df['Description'] = ao_bin_dumps_df['LocalizedDescriptions']
+    item_ids_df['Name'] = ao_bin_dumps_df['LocalizedNames'].map(lambda x:x['ZH-CN'])
+    # item_ids_df['Description'] = ao_bin_dumps_df['LocalizedDescriptions']
     return item_ids_df
 
 item_ids_df = read_items_info()
