851f0ceaaffc29a244e3a80f4cee123a689cb56f
dearfad <dearfad@sina.com>
2023-04-17 11:24:09 +0800

    update

--- aofarmer.py
+++ aofarmer.py
@@ -59,10 +59,13 @@ with col_item_price:
     r = requests.get(search_url)
     prices = pd.DataFrame(r.json())
     st.write(prices[['city','sell_price_min','sell_price_min_date','buy_price_max','buy_price_max_date']])
-    col1, col2, col3 = st.columns(3)
-    col1.metric("Temperature", "70 °F", "1.2 °F")
-    col2.metric("Wind", "9 mph", "-8%")
-    col3.metric("Humidity", "86%", "4%")
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+    col1.metric("Caerleon", "70 °F", "1.2 °F")
+    col2.metric("Bridgewatch", "9 mph", "-8%")
+    col3.metric("Lymhurst", "86%", "4%")
+    col4.metric("Fort Sterling", "70 °F", "1.2 °F")
+    col5.metric("Thetford", "9 mph", "-8%")
+    col6.metric("Marlock", "86%", "4%")
     # st.write(prices)
 
 #     history_hour_url = api_url + 'history/' + item_id + '.json?time-scale=1'
