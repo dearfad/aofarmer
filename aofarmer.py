65f61f7aa00569a41f451f54389c5f6d17ed6d32
dearfad <dearfad@sina.com>
2023-04-17 11:44:22 +0800

    update

--- aofarmer.py
+++ aofarmer.py
@@ -60,7 +60,9 @@ with col_item_price:
     prices = pd.DataFrame(r.json())
     st.write(prices[['city','sell_price_min','sell_price_min_date','buy_price_max','buy_price_max_date']])
     col0, col1, col2, col3, col4, col5, col6 = st.columns(7)
-    col0.metric('**TYPE**', 'sell')
+    with col0:
+        col0.metric('**TYPE**', 'sell')
+        col0.metric('**TYPE**', 'buy')
     col1.metric("**:red[Caerleon]**", prices.loc[prices['city']=='Caerleon', 'sell_price_min'])
     col2.metric("**Bridgewatch**", prices.loc[prices['city']=='Bridgewatch', 'sell_price_min'])
     col3.metric("**Lymhurst**", prices.loc[prices['city']=='Lymhurst', 'sell_price_min'])
