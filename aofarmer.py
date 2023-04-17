1dc5f352d83a98e7b7cf121a3bcec5583d382e8d
dearfad <dearfad@sina.com>
2023-04-17 08:15:35 +0800

    UPDATE

--- aofarmer.py
+++ aofarmer.py
@@ -13,7 +13,7 @@ item_ids_url = 'https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/fo
 image_url = 'https://render.albiononline.com/v1/item/'
 
 
-# @st.cache_data
+@st.cache_data
 def read_items_info():
     ao_bin_dumps = pd.read_json(item_ids_url)
     item_ids = pd.DataFrame()
@@ -25,38 +25,40 @@ def read_items_info():
     return item_ids
 
 item_ids = read_items_info()
-st.write(item_ids)
 
-# st.write(f"*️⃣ **Total: {item_ids.shape[0]}** ➖ 👨‍💼 **By: DEARFAD** ➖")
+st.write(f"*️⃣ **Total: {item_ids.shape[0]}** ➖ 👨‍💼 **By: DEARFAD** ➖")
 
-# col_search, col_result, col_quality = st.columns([1,4,1])
+col_search, col_result, col_quality = st.columns([1,4,1])
 
-# with col_search:
-#     input_name = st.text_input('模糊搜索：', 'ORE')
+with col_search:
+    input_name = st.text_input('模糊搜索：', 'ORE')
 
-# with col_result:
-#     search_result = item_ids[item_ids['UniqueName'].str.contains(input_name.strip(), case=False) | item_ids['Name'].str.contains(input_name.strip(), case=False)]
-#     selected_item = st.selectbox('搜索结果：', search_result['Name'] + ' = ' + search_result['UniqueName'] + ' = ' + search_result['Description'])
+with col_result:
+    search_result = item_ids[item_ids['UniqueName'].str.contains(input_name.strip(), case=False) | item_ids['Name_CN'].str.contains(input_name.strip(), case=False) | item_ids['Name_EN'].str.contains(input_name.strip(), case=False)]
+    if search_result:
+        selected_item = st.selectbox('搜索结果：', search_result['Name_CN'] + ' = ' + search_result['UniqueName'] + ' = ' + search_result['Description_CN'])
+    else:
+        selected_item = st.selectbox('搜索结果', 'T4_BAG')
 
-# with col_quality:
-#     quality = st.selectbox('品质', ('1','2','3','4','5'))
+with col_quality:
+    quality = st.selectbox('品质', ('1','2','3','4','5'))
 
-# col_item_info, col_item_price = st.columns([1,2])
+col_item_info, col_item_price = st.columns([1,2])
 
-# with col_item_info:
-#     name, uniquename, description = selected_item.split(' = ')
-#     item_image_url = image_url + uniquename + '.png' + '?quality=' + quality
-#     st.image(item_image_url)
-#     st.header(name)
-#     st.write(description)
-#     st.write(uniquename)
+with col_item_info:
+    name, uniquename, description = selected_item.split(' = ')
+    item_image_url = image_url + uniquename + '.png' + '?quality=' + quality
+    st.image(item_image_url)
+    st.header(name)
+    st.write(description)
+    st.write(uniquename)
 
-# with col_item_price:
-#     search_url = api_url + 'prices/' + uniquename + '.json?locations=Bridgewatch,Lymhurst,Fort Sterling,Thetford,Martlock,Caerleon&qualities=' + quality
-#     r = requests.get(search_url)
-#     prices = pd.DataFrame(r.json())
-#     # st.write(prices[['city','sell_price_min','buy_price_max']])
-#     st.write(prices)
+with col_item_price:
+    search_url = api_url + 'prices/' + uniquename + '.json?locations=Bridgewatch,Lymhurst,Fort Sterling,Thetford,Martlock,Caerleon&qualities=' + quality
+    r = requests.get(search_url)
+    prices = pd.DataFrame(r.json())
+    # st.write(prices[['city','sell_price_min','buy_price_max']])
+    st.write(prices)
 
 #     history_hour_url = api_url + 'history/' + item_id + '.json?time-scale=1'
 #     r_history_hour = requests.get(history_hour_url)
