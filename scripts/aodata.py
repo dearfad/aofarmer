f551ab11dd0d277b06ead7c83ddbc52bde01e9f8
dearfad <dearfad@sina.com>
2023-04-23 18:07:00 +0800

    update

--- scripts/aodata.py
+++ scripts/aodata.py
@@ -35,7 +35,7 @@ def get_prices(itemlist):
 
     prices = pd.DataFrame()
     history_24 = pd.DataFrame()
-    history_6 = pd.DataFrame()
+    # history_6 = pd.DataFrame()
     for q in query:
         search_url = api_url + 'prices/' + q + '.json?locations=Bridgewatch,Lymhurst,Fort Sterling,Thetford,Martlock,Caerleon'
         r = requests.get(search_url)
@@ -47,17 +47,17 @@ def get_prices(itemlist):
         history_temp = pd.DataFrame(r.json())
         history_24 = pd.concat([history_24, history_temp], ignore_index=True)
 
-        history_6_url = api_url + 'history/' + q + '.json?locations=Bridgewatch,Lymhurst,Fort Sterling,Thetford,Martlock,Caerleon&time-scale=1'
-        r = requests.get(history_6_url)
-        history_temp = pd.DataFrame(r.json())
-        history_6 = pd.concat([history_6, history_temp], ignore_index=True)
+        # history_6_url = api_url + 'history/' + q + '.json?locations=Bridgewatch,Lymhurst,Fort Sterling,Thetford,Martlock,Caerleon&time-scale=1'
+        # r = requests.get(history_6_url)
+        # history_temp = pd.DataFrame(r.json())
+        # history_6 = pd.concat([history_6, history_temp], ignore_index=True)
     
     prices['mergekey'] = prices['item_id'].map(str) + '-' + prices['city'].map(str) + '-' + prices['quality'].map(str)
     history_24['mergekey'] = history_24['item_id'].map(str) + '-' + history_24['location'].map(str) + '-' + history_24['quality'].map(str)
-    history_6['mergekey'] = history_6['item_id'].map(str) + '-' + history_6['location'].map(str) + '-' + history_6['quality'].map(str)
+    # history_6['mergekey'] = history_6['item_id'].map(str) + '-' + history_6['location'].map(str) + '-' + history_6['quality'].map(str)
     prices = prices.merge(history_24, how='left', on='mergekey')
-    prices = prices.merge(history_6, how='left', on='mergekey')
-    st.write(prices.head(5))
+    # prices = prices.merge(history_6, how='left', on='mergekey')
+    # st.write(prices.head(5))
     prices.drop(['mergekey','location','item_id_y','quality_y'],axis=1, inplace=True)
     prices.rename(columns={'item_id_x': 'item_id', 'quality_x': 'quality'}, inplace=True)
     # st.write(prices.data.loc[0])
