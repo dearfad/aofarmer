79cb6d276c1ec9edefa94f6641dfe33246f9ec59
dearfad <dearfad@sina.com>
2023-04-23 08:26:22 +0800

    update

--- aofarmer.py
+++ aofarmer.py
@@ -2,25 +2,11 @@ import streamlit as st
 import pandas as pd
 import requests
 from scripts.category import CATEGORY
+from scripts.aodata import read_item_ids
 
 st.set_page_config(page_title='Albion Online Farmer', page_icon='👨‍🌾', layout="wide")
 
 st.write("# Albion Online Farmer! 👨‍🌾")
-
-api_url = "https://east.albion-online-data.com/api/v2/stats/"
-
-item_ids_url = 'https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/items.json'
-
-@st.cache_data
-def read_item_ids():
-    ao_bin_dumps = pd.read_json(item_ids_url)
-    item_ids = pd.DataFrame()
-    item_ids['UniqueName'] = ao_bin_dumps['UniqueName']
-    item_ids['Name_CN'] = ao_bin_dumps['LocalizedNames'].apply(lambda x:x["ZH-CN"] if x else '')
-    item_ids['Description_CN'] = ao_bin_dumps['LocalizedDescriptions'].apply(lambda x:x["ZH-CN"] if x else '')
-    item_ids['Name_EN'] = ao_bin_dumps['LocalizedNames'].apply(lambda x:x["EN-US"] if x else '')
-    item_ids['Description_EN'] = ao_bin_dumps['LocalizedDescriptions'].apply(lambda x:x["EN-US"] if x else '')
-    return item_ids
 
 item_ids = read_item_ids()
 
---
+++ scripts/aodata.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+api_url = "https://east.albion-online-data.com/api/v2/stats/"
+
+item_ids_url = 'https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/items.json'
+
+@st.cache_data
+def read_item_ids():
+    ao_bin_dumps = pd.read_json(item_ids_url)
+    item_ids = pd.DataFrame()
+    item_ids['UniqueName'] = ao_bin_dumps['UniqueName']
+    item_ids['Name_CN'] = ao_bin_dumps['LocalizedNames'].apply(lambda x:x["ZH-CN"] if x else '')
+    item_ids['Description_CN'] = ao_bin_dumps['LocalizedDescriptions'].apply(lambda x:x["ZH-CN"] if x else '')
+    item_ids['Name_EN'] = ao_bin_dumps['LocalizedNames'].apply(lambda x:x["EN-US"] if x else '')
+    item_ids['Description_EN'] = ao_bin_dumps['LocalizedDescriptions'].apply(lambda x:x["EN-US"] if x else '')
+    return item_ids
