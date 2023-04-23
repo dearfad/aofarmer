ad89940e506f525075bdd1eecbe4de904aca5d9d
dearfad <dearfad@sina.com>
2023-04-23 16:42:42 +0800

    update

--- scripts/aodata.py
+++ scripts/aodata.py
@@ -52,7 +52,7 @@ def get_prices(itemlist):
     prices.drop(['mergekey','location','item_id_y','quality_y'],axis=1, inplace=True)
     prices.rename(columns={'item_id_x': 'item_id', 'quality_x': 'quality'}, inplace=True)
     # st.write(prices.data.loc[0])
-    prices['history'] = prices['data'].apply(lambda x:[x])
+    prices['history'] = prices['data'].apply(lambda x:[x][-1])
     st.write(prices.head(5))        
     return prices
 
