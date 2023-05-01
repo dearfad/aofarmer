247ec7389e42c54dfc58867795e06b538eff0df3
dearfad <dearfad@sina.com>
2023-05-01 13:39:12 +0000

    update

--- scripts/aodata.py
+++ scripts/aodata.py
@@ -55,7 +55,7 @@ def get_ids():
 
 
 def get_prices(server='east', itemlist='T4_BAG', format='json'):
-    query = f'https://{server}.albion-online-data.com/api/v2/stats/Prices/{itemlist}.{format})'
+    query = f'https://{server}.albion-online-data.com/api/v2/stats/Prices/{itemlist}.{format}'
     st.write(query)
     r = requests.get(query)
     prices = pd.DataFrame(r.json())
