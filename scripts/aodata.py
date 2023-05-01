c0cceee82e13ffc3cd258942efa6e29f117e09b5
dearfad <dearfad@sina.com>
2023-05-01 12:30:44 +0000

    update

---
+++ scripts/aodata.py
@@ -0,0 +1,22 @@
+"""
+Filename: aodata.py
+Author: dearfad
+Contact: dearfad@sina.com
+
+Albion Data Project
+site: https://www.albion-online-data.com/
+"""
+
+# API Host URLs
+West_Server_Url = "https://west.albion-online-data.com"
+East_Server_Url = "https://east.albion-online-data.com"
+
+# Item IDs
+Item_IDs_Url = "https://github.com/ao-data/ao-bin-dumps/blob/master/formatted/items.json"
+
+# Location IDs
+Location_IDs_Url = "https://github.com/ao-data/ao-bin-dumps/blob/master/formatted/world.json"
+
+import requests
+import pandas as pd
+
