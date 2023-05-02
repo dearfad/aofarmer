f75f76a056feeeb19ef839e165edcfe1bad6cf49
dearfad <dearfad@sina.com>
2023-05-02 09:46:42 +0000

    update

--- aofarmer.py
+++ aofarmer.py
@@ -22,7 +22,7 @@ st.write(
 )
 
 server = st.selectbox("服务器", ["east", "west"])
-itemlist = st.text_input('模糊搜索：', item_ids['UniqueName'])
+itemlist = st.text_input('模糊搜索：', item_ids['UniqueName'].values)
 
 st.dataframe(get_prices(server=server, itemlist=itemlist), use_container_width=True)
 # st.dataframe(location_ids, use_container_width=True)
